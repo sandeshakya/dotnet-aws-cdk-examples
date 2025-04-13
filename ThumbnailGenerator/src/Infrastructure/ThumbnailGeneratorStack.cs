3bef908d22b42d714a7450aa40366b77590ca2f2
Sandesh Shakya <sandeshakya94@gmail.com>
2025-04-12 21:01:46 -0600

    created Thumbnail Generator solution with CDK and Lambda projects

---
+++ ThumbnailGenerator/src/Infrastructure/ThumbnailGeneratorStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace ThumbnailGenerator
+{
+    public class ThumbnailGeneratorStack : Stack
+    {
+        internal ThumbnailGeneratorStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
